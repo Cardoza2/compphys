4f3699ff6f49a9e7fda67bb2daf3c8a0f035bbcd
BTV25 <70768698+BTV25@users.noreply.github.com>
2024-02-20 10:54:58 -0700

    bug on line 31

--- src/GMRES.jl
+++ src/GMRES.jl
@@ -1,39 +1,41 @@
+using LinearAlgebra
 
+function mygmres(I, b, x0, n, M, A)
+    M_inv = inv(M)
+    A = M_inv*A
+    b = M_inv*b
 
-function mygmres(i, b, x0, n, M, A)
-    #Initialize r
+    v = zeros(length(b),I+1)
+    ω = zeros(length(b))
+    h = zeros(I+1,I)
 
     r0 = b - A*x0
-
-    β = sqrt(sum(r0))
-
-    v[1] = r0/β
+    β = norm(r0)
+    v[:,1] = r0/β
 
     for j=1:I
-        ω[j] = A*v[j]
+        ω .= A*v[:,j]
         for i=1:j
-            h[i,j] = sum(ω[j].*v[i])
-            ω[j] -= h[i,j]*v[j]
+            h[i,j] = dot(ω,v[:,i])
+            ω -= h[i,j]*v[:,j]
         end
-        h[j+1,j] = sqrt(sum(ω[j]))
+        h[j+1,j] = norm(ω)
         if h[j+1,j] == 0
             I = j
             break
         end
-        v[j+1] = ω[j]/h[j+1,j]
+        v[:,j+1] = ω/h[j+1,j]
     end
-    
+    e1 = zeros(length(b))
+    e1[1] = 1
     ym = h\(β*e1)
     xm = x0 + Vm*ym
-
     return xm
-
 end
 
+A = rand(4,4)
+b = rand(4)
+x0 = rand(4)
 
-
-
-function arnoldi(A, x0, m)
-
-    return h, v
-end
+first = A\b
+second = mygmres(10,b,x0,4,I(4),A)
