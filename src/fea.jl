809a0553cbd51c6ae24e5ec20bee21c3195e03e2
cardoza2 <adam@cardoza.one>
2024-02-20 11:53:56 -0700

    Intitial FEA code

---
+++ src/fea.jl
@@ -0,0 +1,38 @@
+
+
+
+"""
+    hatfunction(x, i, mesh)
+
+Evaluate the ith hat function at x. 
+
+i should range from 2, to M+1 (total mesh length is M+2)
+"""
+function hatfunction(x, i, mesh)
+
+    if i==1
+        error("hatfunction(): i should never equal 1.")
+    elseif i==length(mesh)
+        n_mesh = length(mesh)
+        error("hatfunction(): i should never equal $n_mesh. ")
+    end
+
+    if mesh[i-1]<=x
+        return (x-mesh[i-1])/(mesh[i]-mesh[i-1])
+    elseif x<mesh[i+1]
+        return 1 - (x-mesh[i])/(mesh[i+1]-mesh[i])
+    else
+        return 0
+    end
+end
+
+
+"""
+    meshproblem(; domain=(0,1), n_elems=50)
+
+Create the vector across the domain. (The mesh is a vector across the domain). 
+"""
+function meshproblem(;domain=(0,1), n_elems=50)
+    mesh = range(domain..., length=n_elems )
+    return collect(mesh)
+end
