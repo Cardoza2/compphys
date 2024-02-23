0a5d3789039435cdb399300020f55c24efcbd1e2
BTV25 <70768698+BTV25@users.noreply.github.com>
2024-02-23 12:45:23 -0700

    update gmres to accept A and M as either sparse or dense

--- src/GMRES.jl
+++ src/GMRES.jl
@@ -2,37 +2,39 @@ using LinearAlgebra
 
 """
     mygmres(iters, b, x0, n, M, A)
-Use the GMRES algorithm to solve a linear system of equations. 
+Use the GMRES algorithm to solve a linear system of equations.
 
 **Inputs**
 - iters::Int - The maximum number of iterations to take.
-- b::Vector{Float} - The constants of the system. 
+- b::Vector{Float} - The constants of the system.
 - x0::Vector{Float} - The initial guess of the solution.
-- n::Int -  The dimension of the problem. 
+- n::Int -  The dimension of the problem.
 - M::Matrix{Float} - The left-preconditioner matrix.
-- A::Matrix{Float} - The coefficients of the system. 
+- A::Matrix{Float} - The coefficients of the system.
 """
 function mygmres(iters, b, x0, n, M, A; tolerance=1e-6, verbose::Bool=false)
     ### Preconditioning
-    M_inv = inv(M)
-    A = M_inv*A
-    b = M_inv*b
+    M_temp = zeros(size(M))
+    M_temp .= M
+    M_inv = inv(M_temp)
+    A = sparse(M_inv*A)
+    b = sparse(M_inv*b)
 
     x_sol = deepcopy(x0)
 
     ### Initialize data storage.
-    n = length(b) 
+    n = length(b)
     v = zeros(n,iters+1) #Krylov vectors
     omega = zeros(n)
     h = zeros(iters+1,iters) #Hessenberg matrix
     r0 = zeros(n) #Residual vector
     ym = zeros(iters+1)
 
-    e1 = zeros(iters+1) #Error vector? 
+    e1 = zeros(iters+1) #Error vector?
     e1[1] = 1
 
     for i = 2:iters #todo: skipping some might be more efficient
-        v_i = view(v, :, 1:i+1) 
+        v_i = view(v, :, 1:i+1)
         h_i = view(h, 1:i+1, 1:i)
         e_i = view(e1, 1:i+1)
         ym_i = view(ym, 1:i)
@@ -60,18 +62,18 @@ end
 """
     mygmres!(v, h, omega, ...)
 
-The Arnoldi iteration algorithm to generate the Krylov vectors. Note it generates the vectors in place. 
+The Arnoldi iteration algorithm to generate the Krylov vectors. Note it generates the vectors in place.
 
 **Inputs**
 """
 function Arnoldi!(v, h, omega, r0, x0, A, b, iters, current_iter)
-    
-    ### Initial errors and Krylov vector. 
+
+    ### Initial errors and Krylov vector.
     r0 .= b - A*x0
     beta = norm(r0)
     v[:,1] = r0/beta
 
-    ### Arnoldi Iteration to get the Krylov vectors. 
+    ### Arnoldi Iteration to get the Krylov vectors.
     for j=current_iter:iters
         omega .= A*v[:,j]
 
@@ -82,7 +84,7 @@ function Arnoldi!(v, h, omega, r0, x0, A, b, iters, current_iter)
         h[j+1,j] = norm(omega)
 
         #Avoid dividing by zero
-        if isapprox(h[j+1,j], 0) 
+        if isapprox(h[j+1,j], 0)
             iters = j
             break
         end
